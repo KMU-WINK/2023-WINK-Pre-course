9dc5bd769b2defa5617aba67498d4a94bad7b6d1
cg10036 <cg10036@gmail.com>
2023-02-11 01:13:56 +0900

    a

--- TodoList/index.js
+++ TodoList/index.js
@@ -1,18 +1,33 @@
 const addTodo = () => {
-  let textbox = document.querySelector("#textbox");
+  let text = document.querySelector("#textbox").value;
+  if (!text) {
+    alert("빈칸입니다");
+  } else {
+    let todo = document.createElement("div");
+    todo.classList.add("todo-item");
 
-  let todo = document.createElement("div");
-  todo.classList.add("todo-item");
+    let checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("onclick", "todoDone(this)");
 
-  let input = document.createElement("input");
-  input.setAttribute("type", "checkbox");
-  input.setAttribute("onclick", "todoDone()");
-  todo.append(input, textbox.value);
+    let button = document.createElement("input");
+    button.setAttribute("type", "button");
+    button.setAttribute("onclick", "delTodo(this)");
+    button.setAttribute("value", "❌");
 
-  let todoContent = document.querySelector(".todo-content");
-  todoContent.append(todo);
+    todo.append(checkbox, text, button);
+
+    let todoContent = document.querySelector(".todo-content");
+    todoContent.append(todo);
+  }
+};
+
+const delTodo = (el) => {
+  el.parentElement.remove();
 };
 
 const todoDone = (el) => {
-  //   el.parentElement.classList.toggle // <- 클래스 이름으로 선택 시 밑줄가게
+  //   el.checked;
+  //   el.parentElement.classList.toggle; // <- 클래스 이름으로 선택 시 밑줄가게
+  el.parentElement.classList.toggle("done");
 };
