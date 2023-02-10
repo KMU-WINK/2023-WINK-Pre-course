363eaed37e32a0fbedd66122a190ad2442e11c6e
cg10036 <cg10036@gmail.com>
2023-02-11 01:04:00 +0900

    add todo done

--- TodoList/index.js
+++ TodoList/index.js
@@ -6,8 +6,13 @@ const addTodo = () => {
 
   let input = document.createElement("input");
   input.setAttribute("type", "checkbox");
+  input.setAttribute("onclick", "todoDone()");
   todo.append(input, textbox.value);
 
   let todoContent = document.querySelector(".todo-content");
   todoContent.append(todo);
+};
+
+const todoDone = (el) => {
+  //   el.parentElement.classList.toggle // <- 클래스 이름으로 선택 시 밑줄가게
 };
