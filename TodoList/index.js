b379b9737db4335118b32bf03515448ac1dd34eb
cg10036 <cg10036@gmail.com>
2023-02-19 15:50:12 +0900

    🥕

--- TodoList/index.js
+++ TodoList/index.js
@@ -1,3 +1,31 @@
+window.onload = () => {
+  let week = [
+    "일요일",
+    "월요일",
+    "화요일",
+    "수요일",
+    "목요일",
+    "금요일",
+    "토요일",
+  ];
+  let today = new Date();
+
+  let year = today.getFullYear();
+  let month = today.getMonth() + 1;
+  let day = today.getDate();
+  let dayName = week[today.getDay()];
+
+  document.querySelector(".date").append(`${year}년 ${month}월 ${day}일`);
+  document.querySelector(".day").append(dayName);
+
+  reloadCount();
+};
+
+const reloadCount = () => {
+  let count = document.querySelectorAll(".todo-item:not(.done)").length;
+  document.querySelector(".count").innerText = count;
+};
+
 const addTodo = () => {
   let text = document.querySelector("#textbox").value;
   if (!text) {
@@ -19,15 +47,17 @@ const addTodo = () => {
 
     let todoContent = document.querySelector(".todo-content");
     todoContent.append(todo);
+
+    reloadCount();
   }
 };
 
 const delTodo = (el) => {
   el.parentElement.remove();
+  reloadCount();
 };
 
 const todoDone = (el) => {
-  //   el.checked;
-  //   el.parentElement.classList.toggle; // <- 클래스 이름으로 선택 시 밑줄가게
   el.parentElement.classList.toggle("done");
+  reloadCount();
 };
